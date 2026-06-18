a126d669595f40bba6fdd7d4d0d968c0fedc3924
baophan245 <01666119817fz@gmail.com>
2026-06-18 23:29:44 +0700

    d

--- src/test/java/seat/SeatRepositoryPerformanceTest.java
+++ src/test/java/seat/SeatRepositoryPerformanceTest.java
@@ -24,7 +24,7 @@ public class SeatRepositoryPerformanceTest {
         long end = System.currentTimeMillis();
 
         long duration = end - start;
-
+        
         assertEquals(10000, loaded.size());
         assertTrue(duration <= 500, "Performance requirement not met! Duration: " + duration + "ms");
     }
